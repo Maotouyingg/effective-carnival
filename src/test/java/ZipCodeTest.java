977d19193500b8a09dc66bf812a7cdb6f337facc
maotouyingg <timofeykotov592@gmail.com>
2025-09-23 12:18:02 +0300

    0.4 all test completely works!
    >Added WebDriverWait to @BeforeMethod to avoid creating it everytime in tests
    >Added a wait for the first_name field to appear
    >Added a wait for the zip code field in both tests

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -1,46 +1,52 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+
 public class ZipCodeTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Test
     public void checkZipCode4digits() {
-        {
-
-            {
-                //System.setProperty("webdriver.chrome.driver", "timofeyfilin@MacBook-Air-Timofej Google Chrome.app");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://www.sharelane.com/cgi-bin/register.py");
-                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-                wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("zip_code")));
-                driver.findElement(By.name("zip_code")).sendKeys("1234");
-                driver.findElement(By.cssSelector("[value = 'Continue']")).click();
-
-            }
-        }
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("zip_code")));
+        driver.findElement(By.name("zip_code")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[value = 'Continue']")).click();
     }
+
     @Test
+    public void CheckZipCodeValidTest() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
 
-    public void CheckZipCodeValidTest(){}
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("zip_code")));
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[value = 'Continue']")).click();
 
-    public void setUp() {
-        ChromeDriver driver = new ChromeDriver();
-        {
-            driver.findElement(By.name("zip_code")).sendKeys("12345");
-            driver.findElement(By.cssSelector("[value = 'Continue']")).click();
-            driver.findElement(By.name("first_name")).sendKeys("Timofey");
-            driver.findElement(By.name("last_name")).sendKeys("Filin");
-            driver.findElement(By.name("email")).sendKeys("tfilin888@ya.ru");
-            driver.findElement(By.name("password1")).sendKeys("Password123!");
-            driver.findElement(By.name("password2")).sendKeys("Password123!");
-            driver.findElement(By.cssSelector("[value = 'Register']")).click();
-        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("first_name")));
+
+        driver.findElement(By.name("first_name")).sendKeys("Timofey");
+        driver.findElement(By.name("last_name")).sendKeys("Filin");
+        driver.findElement(By.name("email")).sendKeys("tfilin888@ya.ru");
+        driver.findElement(By.name("password1")).sendKeys("Password123!");
+        driver.findElement(By.name("password2")).sendKeys("Password123!");
+        driver.findElement(By.cssSelector("[value = 'Register']")).click();
     }
 }
